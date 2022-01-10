a2c150fc44bde4c22aa48ab9d7296a078948c9e8
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-01-10 13:39:32 +0200

    added bmi model

---
+++ BMI Calculator/Model/BMIModel.swift
@@ -0,0 +1,14 @@
+//
+//  BMIModel.swift
+//  BMI Calculator
+//
+//  Created by Abdulrahman on 1/10/22.
+//
+
+import UIKit
+
+struct BMIModel {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
